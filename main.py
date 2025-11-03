bbb2c2004742fa6bed9ba227be7368672034bc5f
Oliver Clissold <ollyroo@outlook.com.au>
2025-11-03 14:59:59 +1100

    webook

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         payload = request.get_json()
