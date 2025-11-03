fef6035a32683aa100859e8ce4655b0d6ae5cb39
Oliver Clissold <ollyroo@outlook.com.au>
2025-11-03 15:29:53 +1100

    Liaten on all IPs

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/webook', methods=['POST'])
+@app.route('/webook', methods=['POST','GET'])
 def webhook():
     if request.method == 'POST':
         payload = request.get_json()
         print(payload)
         # Here you can add your business logic to process the payload
         return 'Webhook received successfully', 200
+    if request.method == 'GET':
+        return 'Webhook endpoint is live', 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=3000, host="0.0.0.0")
